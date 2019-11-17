2291d42547bfe02a99c1d608be436b1e78e558ee
Tolga Egilmezel <tolga@egilmezel.com>
2019-11-17 22:20:38 +0300

    11ty testing

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,15 @@
-module.exports = {
-  dataTemplateEngine: "njk",
-  dir: {
-    input: ".",
-    output: "_site"
+module.exports = config => {
+
+  const _scss = require('./config/_scss');
+  _scss('./scss/main.scss');
+
+  config.addPassthroughCopy("css");
+  return {
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    dir: {
+      input: ".",
+      output: "_site"
+    }
   }
-}
+};
